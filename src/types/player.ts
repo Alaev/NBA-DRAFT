b78d2a22512be034ae375899898bf14a09be1447
Michael Alaev <alaev.michael@gmail.com>
2020-12-01 18:48:03 +0200

    added types

---
+++ src/types/player.ts
@@ -0,0 +1,25 @@
+
+export interface Team {
+  id: number;
+  abbreviation: string;
+  city: string;
+  conference: string;
+  division: string;
+  full_name: string;
+  name: string;
+}
+
+export interface Player {
+  id: number;
+  first_name: string;
+  height_feet: number | null;
+  height_inches: number | null;
+  last_name: string;
+  position: string;
+  team: Team;
+  weight_pounds: number | null;
+}
+
+interface Props {
+  player: Player;
+}
