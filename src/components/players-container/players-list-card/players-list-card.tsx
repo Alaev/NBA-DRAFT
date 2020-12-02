c4ea1a519ac7f7dc877195b25d600168e23bd2c9
Michael Alaev <alaev.michael@gmail.com>
2020-12-02 15:27:48 +0200

    Changes:
    Added memo to remove re renders after finding them in the profiler
    Renamed component to keep consistent
    Added more info about the player with styles

--- src/components/players-container/players-list-card/players-list-card.tsx
+++ src/components/players-container/players-list-card/players-list-card.tsx
@@ -11,8 +11,8 @@ interface Props {
   player: Player;
 }
 
-export default function PlayerCard({ player }: Props) {
-  const { first_name, last_name, favorite } = player;
+export default React.memo(function PlayerCard({ player }: Props) {
+  const { first_name, last_name, favorite, height_feet, weight_pounds, position, team } = player;
   const setPlayersStore = useSetRecoilState(playersStore);
 
   const toggleFavorite = () => {
@@ -32,8 +32,29 @@ export default function PlayerCard({ player }: Props) {
   };
 
   return (
-    <div className="flex px-2 py-4 my-2 border bg-white border-gray-200 hover:border-gray-400 hover:shadow-sm cursor-pointer ">
-      {first_name}, {last_name}
+    <div className="flex flex-row p-4 my-2 border bg-white border-gray-200 hover:border-gray-400 hover:shadow-sm cursor-pointer ">
+      <div className="w-full">
+        <div className="text-blue-900 flex mb-2 space-x-14">
+          <p>
+            {' '}
+            {first_name}, {last_name}{' '}
+          </p>
+          <p className="text-black">
+            Team: <span className="text-blue-900">{team.name}</span>
+          </p>
+        </div>
+        <div className="flex space-x-8">
+          <p>
+            Height: <span className="text-yellow-500">{height_feet || '-'}</span>{' '}
+          </p>
+          <p>
+            Wight: <span className="text-yellow-500">{weight_pounds || '-'}</span>
+          </p>
+          <p>
+            Position: <span className="text-yellow-500">{position}</span>
+          </p>
+        </div>
+      </div>
       <Button className="ml-auto bg-none" onClick={handleFavorite}>
         <Icon className="shadow-sm hover:shadow-md hover:bg-yellow-400 rounded-full">
           <HeartIcon isFavorite={favorite}></HeartIcon>
@@ -41,4 +62,4 @@ export default function PlayerCard({ player }: Props) {
       </Button>
     </div>
   );
-}
+});
